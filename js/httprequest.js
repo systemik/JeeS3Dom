474292a3c95f92472eaac54e11d5e999aa832893
systemic <systemic@UpInTheAir.local>
2017-04-04 19:53:21 +0200

    Swipe concept and Json retrieve (to be coded properly afterward)

--- js/httprequest.js
+++ js/httprequest.js
@@ -1,11 +1,79 @@
 // Load Box value on start
+// Indiquer l'adresse IP/url et la clé jeedom
 
 function initJeedomValue() {
-localStorage.setItem("IP", "192.168.1.x");
-localStorage.setItem("KEY", "2ycN5TGJasfakjghPaK8");
+	localStorage.setItem("IP", "192.168.1.x");
+	localStorage.setItem("KEY", "2xcN5TGJ1fXAkjghPaK8");
 }
 
+//Load Box JSON value on start
+//Indiquer l'adresse IP/url et la clé jeedom
+
+function initJeedomJsonValue() {
+	
+var myKeyValue, myIPValue;
+	
+myKeyValue =  localStorage.getItem("KEY");
+myIPValue =  localStorage.getItem("IP");
+console.log("KEY Value: " + myKeyValue);
+console.log("IP Value: " + myIPValue);
+
+var xhttp1;
+var xhttp2;
+var xhttp3;
+
+xhttp1=new XMLHttpRequest();
+xhttp1.open("POST", "http://" + myIPValue + "/core/api/jeeApi.php");
+xhttp1.setRequestHeader("Content-Type", "application/text/plain");
+xhttp1.send(JSON.stringify({"jsonrpc": "2.0", "method":"object::all","params":{"apikey": "2xcN5TGJ1fXAkjghPaK8"}, "id":1}));
+// curl -d '{"jsonrpc": "2.0", "method":"object::all","params":{"apikey": "2xcN5TGJ1fXAkjghPaK8"}, "id":1}' http://192.168.1.x/core/api/jeeApi.php
+
+
+		xhttp1.onreadystatechange = function() {
+			if (xhttp1.readyState == 4 && xhttp1.status == 200) {
+				var data = JSON.parse(xhttp1.responseText);
+				console.log(JSON.stringify(data));
+				localStorage.setItem("JSON-Objects", JSON.stringify(data));
+			}
+		}
+
+xhttp2=new XMLHttpRequest();
+
+xhttp2.open("POST", "http://" + myIPValue + "/core/api/jeeApi.php");
+xhttp2.setRequestHeader("Content-Type", "application/text/plain");
+xhttp2.send(JSON.stringify({"jsonrpc": "2.0", "method":"cmd::all","params":{"apikey": "2xcN5TGJ1fXAkjghPaK8"}, "id":1}));
+// curl -d '{"jsonrpc": "2.0", "method":"object::all","params":{"apikey": "2xcN5TGJ1fXAkjghPaK8"}, "id":1}' http://192.168.1.x/core/api/jeeApi.php
+
+
+		xhttp2.onreadystatechange = function() {
+			if (xhttp2.readyState == 4 && xhttp2.status == 200) {
+				var data = JSON.parse(xhttp2.responseText);
+				console.log(JSON.stringify(data));
+				localStorage.setItem("JSON-Commands", JSON.stringify(data));
+			}
+		}
+				
+xhttp3=new XMLHttpRequest();
+
+xhttp3.open("POST", "http://" + myIPValue + "/core/api/jeeApi.php");
+xhttp3.setRequestHeader("Content-Type", "application/text/plain");
+xhttp3.send(JSON.stringify({"jsonrpc": "2.0", "method":"scenario::all","params":{"apikey": "2xcN5TGJ1fXAkjghPaK8"}, "id":1}));
+// curl -d '{"jsonrpc": "2.0", "method":"object::all","params":{"apikey": "2xcN5TGJ1fXAkjghPaK8"}, "id":1}' http://192.168.1.x/core/api/jeeApi.php
+
+
+		xhttp3.onreadystatechange = function() {
+			if (xhttp3.readyState == 4 && xhttp3.status == 200) {
+				var data = JSON.parse(xhttp3.responseText);
+				console.log(JSON.stringify(data));
+				localStorage.setItem("JSON-Scenario", JSON.stringify(data));
+			}
+		}
+}
+
+
 // Basic HTTP request to launch command
+// Les commandes sont en http. On peut mettre https si besoin.
+
 
 function requestsite(jeedomcmd)
  {

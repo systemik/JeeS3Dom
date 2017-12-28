20400dec9b7d202be67275d59f4c14f747dcc529
systemik <sebastien2@laverriere.org>
2017-12-28 18:08:53 +0100

    Add Swipe code based on Didier3L code

--- js/list_swipelist.js
+++ js/list_swipelist.js
@@ -25,5 +25,42 @@
 		// release object
 		swipeList.destroy();
 	});
+	
+    /* ------------------ salon ---------------------------------------- */
+    
+    var salonSwipe = document.getElementById("salon-swipe");
+    
+    salonSwipe.addEventListener("swipelist.left", function(evt){
+    	requestcommand(436);
+    });
 
+    salonSwipe.addEventListener("swipelist.right", function(evt){
+    	requestcommand(437);
+    });
+    
+    /* ------------------ enfant1 ---------------------------------------- */
+    
+    var enfant1Swipe = document.getElementById("enfant1-swipe");
+    
+    enfant1Swipe.addEventListener("swipelist.left", function(evt){
+    	requestcommand(667);
+    });
+
+    enfant1Swipe.addEventListener("swipelist.right", function(evt){
+    	requestcommand(668);
+    });
+	
+    /* ------------------ enfant2 ---------------------------------------- */
+    
+    var enfant2Swipe = document.getElementById("enfant2-swipe");
+    
+    enfant2Swipe.addEventListener("swipelist.left", function(evt){
+    	requestcommand(427);
+    });
+
+    enfant2Swipe.addEventListener("swipelist.right", function(evt){
+    	requestcommand(428);
+    });
+    
+	
 }());
